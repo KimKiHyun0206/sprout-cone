1cd7a0260c7c1bfb2587c50594c554eb891bc960
Duskafka <rlarlgus0206@naver.com>
2025-11-16 11:18:33 +0900

    init

---
+++ src/main/java/com/sproutcone/SproutConeApplication.java
@@ -0,0 +1,13 @@
+package com.sproutcone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutConeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SproutConeApplication.class, args);
+    }
+
+}
